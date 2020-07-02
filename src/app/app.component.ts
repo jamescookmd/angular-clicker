32c056b06876c4e3cb8a64debddc93f86ee07af4
jamescookmd <james@cookmd.com>
2020-07-01 20:46:56 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  bytes = 1;
+  kilos = 2;
+  megas = 3;
+  byteClickerValue = 0;
+
+  constructor() {
+    // Start game loop.
+    setInterval(() => {
+      this.bytes += this.byteClickerValue;
+    }, 1000);
+  }
+
+  makeByte() {
+    this.bytes += 1;
+  }
+
+  makeKilo() {
+    this.bytes -= 10;
+    this.kilos += 1;
+  }
+
+  disableKilos() {
+    return this.bytes < 10;
+  }
+
+  startClicker() {
+    this.byteClickerValue += 1;
+  }
+}
